a2232e4573dcdf03326d3c7e234e9aa9fd326532
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-03-28 13:20:34 -0500

    initalizing stored functions and procedures

---
+++ CarDealership_stored_funcs_procedures.sql
@@ -0,0 +1,18 @@
+-- CAR DEALERSHIP STORED ACTIONS --
+
+
+-- >> STORED PROCEDURES << 
+-- 1. When a car is purchased:
+--        remove from car_on_lot and add to car_purchased
+
+
+-- >> STORED FUNCTIONS << 
+-- 1. Add data to a table 
+-- 2. Look up: how many cars there are of a certain make
+-- 3. Look up: service history by ticket_id OR appt_id
+-- 4. Loop up: customer info from car purchased 
+-- 5. Look up: car info from customer 
+-- 6. Look up: mechanics who worked on any given invoice
+-- 7. Look up: salesperson who helped with car purchase
+
+
